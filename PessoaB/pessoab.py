466287a5363eb6320f104aaaf8b1c2350daa61b3
Garc1a <matheusgo970@gmail.com>
2026-08-11 20:11:54 -0300

    feat:Adicionei a criacao da tabela de precos no banco de dados

--- PessoaB/pessoab.py
+++ PessoaB/pessoab.py
@@ -15,5 +15,22 @@ cursor.execute("""
 
 conexao.commit()
 
+def inserir_preco(dado):
+    cursor.execute(
+        "INSERT INTO precos (produto, preco, data_coleta) VALUES (?, ?, ?)",
+        (dado["produto"], dado["preco"], dado["data_coleta"])
+    )
+    conexao.commit()
+    print(f"Preço de '{dado['produto']}' salvo: R$ {dado['preco']}")
+
 print("Banco de dados conectado.")
 print("Tabela 'precos' pronta para uso.")
+
+# Teste - simula o que a Pessoa A vai entregar
+dado_teste = {
+    "produto": "Notebook Dell",
+    "preco": 3499.90,
+    "data_coleta": "2026-08-11"
+}
+
+inserir_preco(dado_teste)
